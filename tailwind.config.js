313bc378f2e1557d69b76b1fc50c639cd519b2fc
harshsahani14 <117151304+harshsahani14@users.noreply.github.com>
2023-10-28 20:03:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+      white: "#fff",
+      gray600: "rgb(75 85 99)",
+      gray400: " rgb(148 163 184)",
+      gray300: "rgb(203 213 225)",
+      green500: "rgb(34 197 94)",
+      borderslate700: "rgb(51 65 85)",
+      bordergray900 : "rgb(17 24 39)",
+      gray300: " rgb(209 213 219)",
+      slate800:" rgb(30 41 59)",
+      neutral700: "rgb(64 64 64)",
+      green:"#04AF70",
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    }
+  },
+  plugins: [],
+}
+
